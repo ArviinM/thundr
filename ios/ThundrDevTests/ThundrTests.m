9299812859836436845c31a9298c587e59de2907
ArviinM <rhenmedina20@gmail.com>
2023-09-28 18:54:02 +0800

    Testing

--- ios/ThundrDevTests/ThundrTests.m
+++ ios/ThundrDevTests/ThundrTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ThundrTests : XCTestCase
+@interface ThundrDevTests : XCTestCase
 
 @end
 
-@implementation ThundrTests
+@implementation ThundrDevTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
