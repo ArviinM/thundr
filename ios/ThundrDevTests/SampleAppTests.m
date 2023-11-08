de962a7c13b6cda6a30be49bbf6b8965b525710f
ArviinM <rhenmedina20@gmail.com>
2023-11-09 00:52:50 +0800

    Fixed build crashing

--- ios/ThundrDevTests/SampleAppTests.m
+++ ios/ThundrDevTests/SampleAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SampleAppTests : XCTestCase
+@interface ThundrDevTests : XCTestCase
 
 @end
 
-@implementation SampleAppTests
+@implementation ThundrDevTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
