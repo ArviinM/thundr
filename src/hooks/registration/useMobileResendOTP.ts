749fda9957a5e61b33040227551d3f4b4aabbabd
ArviinM <rhenmedina20@gmail.com>
2024-04-12 10:49:16 +0800

    [CHORE] - Added proper error handling with response status.

--- src/hooks/registration/useMobileResendOTP.ts
+++ src/hooks/registration/useMobileResendOTP.ts
@@ -25,6 +25,7 @@ export function useMobileResendOTP() {
           status: response.data.status,
           message: response.data.message,
           data: response.data.data,
+          statusCode: response.status,
         } as Error;
       }
 
