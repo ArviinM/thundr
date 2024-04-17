d03c72dce743521e61fdafbca25b26d5fd9cc32e
ArviinM <rhenmedina20@gmail.com>
2024-04-17 10:42:33 +0800

    [CHORE] - WIP - Home Drawer

---
+++ src/navigation/Private/Home/Drawer/HomeDrawer.tsx
@@ -0,0 +1,9 @@
+//TODO: Create Home Drawer after build
+import React from 'react';
+import {View} from 'react-native';
+
+const HomeDrawer = () => {
+  return <View />;
+};
+
+export default HomeDrawer;
