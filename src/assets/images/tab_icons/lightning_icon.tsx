bb8fb271289b76f66ad7314e93582c7521530126
ArviinM <rhenmedina20@gmail.com>
2024-04-19 17:18:51 +0800

    [CHORE] - Added Svgs to enhance icons dpi.

---
+++ src/assets/images/tab_icons/lightning_icon.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Svg, {Defs, LinearGradient, Path, Stop} from 'react-native-svg';
+export const LightningIcon = (props: {focused: boolean}) => {
+  return (
+    <Svg width={21} height={30} fill="none">
+      {props.focused ? (
+        <>
+          <Path
+            fill="url(#a)"
+            fillRule="evenodd"
+            d="M8.414 29.751a1.56 1.56 0 0 1-.613-1.864V17.869H2.053a1.56 1.56 0 0 1-1.271-2.617L10.204.498a1.56 1.56 0 0 1 2.634 1.518v10.552h5.726a1.56 1.56 0 0 1 1.232 2.654l-9.43 14.318a1.56 1.56 0 0 1-1.952.211Z"
+            clipRule="evenodd"
+          />
+          <Defs>
+            <LinearGradient
+              id="a"
+              x1={10.307}
+              x2={10.307}
+              y1={30}
+              y2={0}
+              gradientUnits="userSpaceOnUse">
+              <Stop stopColor="#FDAB2D" />
+              <Stop offset={1} stopColor="#EA3E4D" />
+            </LinearGradient>
+          </Defs>
+        </>
+      ) : (
+        <>
+          <Path
+            fill="#8E8E8F"
+            fillRule="evenodd"
+            d="M8.414 29.751a1.56 1.56 0 0 1-.613-1.864V17.869H2.053a1.56 1.56 0 0 1-1.271-2.617L10.204.498a1.56 1.56 0 0 1 2.634 1.518v10.552h5.726a1.56 1.56 0 0 1 1.232 2.654l-9.43 14.318a1.56 1.56 0 0 1-1.952.211Z"
+            clipRule="evenodd"
+          />
+        </>
+      )}
+    </Svg>
+  );
+};
