1b798b8c6c4de2b563afacca8cbeb5da419a2f0d
ArviinM <rhenmedina20@gmail.com>
2024-05-01 03:11:15 +0800

    chore: Added ThundrBolt - No Pan Gestures for Now ```

---
+++ src/screens/Private/ThundrBolt/ThundrBolt.tsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {COLORS, SIZES, width} from '../../../constants/commons.ts';
+import {BoltLogo} from '../../../assets/images/thundrbolt_icons/BoltLogo.tsx';
+import {scale} from '../../../utils/utils.ts';
+import GradientButton from '../../../components/shared/GradientButton.tsx';
+
+const ThundrBolt = () => {
+  return (
+    <SafeAreaView
+      edges={['left', 'right', 'bottom']}
+      style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: COLORS.white,
+          paddingHorizontal: 20,
+          paddingVertical: 30,
+          alignItems: 'center',
+        }}>
+        <View style={{flexDirection: 'column', alignItems: 'center'}}>
+          <BoltLogo />
+          <Text
+            style={{
+              textAlign: 'center',
+              marginVertical: 10,
+              color: COLORS.gray,
+              fontFamily: 'Montserrat-Medium',
+              fontSize: scale(11),
+            }}>
+            Paid subscription para sa mga{'\n'}kabog. Unlock exclusive access
+            {'\n'}to all features.
+          </Text>
+        </View>
+        <View style={{paddingVertical: 20, alignItems: 'center'}}>
+          <Text
+            style={{
+              textAlign: 'center',
+              marginVertical: 10,
+              color: COLORS.primary1,
+              fontFamily: 'Montserrat-Black',
+              fontSize: scale(18),
+            }}>
+            WHAT'S IN IT FOR YOU?{'\n'}LET ME TELL YOU, MARS!
+          </Text>
+        </View>
+        {/* Gestures Here Swipe */}
+      </View>
+      <View style={{alignItems: 'center', width: '100%'}}>
+        {/* Set width to 100% */}
+        <View
+          style={{
+            flexDirection: 'row',
+            width: '100%',
+          }}>
+          {/*terms pickers*/}
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              borderWidth: 1.5,
+              margin: scale(13),
+              paddingHorizontal: scale(20),
+              paddingVertical: scale(12),
+              borderRadius: 10,
+              borderColor: COLORS.primary1,
+            }}>
+            {/*yearly*/}
+            <View>
+              <View>
+                <Text
+                  style={{
+                    color: COLORS.primary1,
+                    fontFamily: 'Montserrat-Black',
+                    fontSize: scale(16),
+                    paddingVertical: 3,
+                  }}>
+                  Yearly
+                </Text>
+              </View>
+              <Text
+                style={{
+                  color: COLORS.primary1,
+                  fontFamily: 'Montserrat-Medium',
+                  fontSize: scale(14),
+                  paddingVertical: 3,
+                }}>
+                ₱3,000
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              // borderWidth: 1.5,
+              margin: scale(13),
+              paddingHorizontal: scale(20),
+              paddingVertical: scale(12),
+              borderRadius: 10,
+              // borderColor: COLORS.primary1,
+              backgroundColor: '#F3F4F7',
+            }}>
+            {/*monthly*/}
+            <View>
+              <Text
+                style={{
+                  color: COLORS.primary1,
+                  fontFamily: 'Montserrat-Black',
+                  fontSize: scale(16),
+                  paddingVertical: 3,
+                }}>
+                Monthly
+              </Text>
+
+              <Text
+                style={{
+                  color: COLORS.primary1,
+                  fontFamily: 'Montserrat-Medium',
+                  fontSize: scale(14),
+                  paddingVertical: 3,
+                }}>
+                ₱300
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {/*Subscribe Button Here*/}
+          <GradientButton
+            onPress={() => console.log('Open navigation')}
+            text="SUBSCRIBE"
+            buttonStyle={styles.buttonStyle}
+            textStyle={styles.buttonTextStyle}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ThundrBolt;
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    alignItems: 'center',
+    // maxWidth: width,
+    width: width - 64,
+    height: 50,
+    justifyContent: 'center',
+    borderRadius: 30,
+    marginTop: 12,
+  },
+  buttonTextStyle: {
+    letterSpacing: -0.4,
+    fontFamily: 'Montserrat-Bold',
+    color: COLORS.white,
+    fontSize: SIZES.h5,
+  },
+});
